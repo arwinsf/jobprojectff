202a5233e2f00902ddedfa93ac3ed8774116f56a
Arwin Fattahy <arwin.fattahy@gmail.com>
2021-02-25 07:32:47 +0100

    added correct fetch and cleaned up stuff

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TitleComponent></TitleComponent>
+    <FetchComponent></FetchComponent>
+    <GIFComponent></GIFComponent>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TitleComponent from "@/components/TitleComponent";
+import FetchComponent from "@/components/FetchComponent";
+import GIFComponent from "@/components/GIFComponent";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GIFComponent,
+    TitleComponent,
+    FetchComponent
   }
 }
 </script>
 
 <style>
+html {
+  background-color: #2a59c0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  background-color: #2a59c0;
+  color: white;
+  text-align: center;
+  font-family: "Roboto Light", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
