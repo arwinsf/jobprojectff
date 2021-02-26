52c1a7fc7c77accb13a18701a90298dab0ca1572
Arwin Fattahy <arwin.fattahy@gmail.com>
2021-02-26 14:20:59 +0100

    cleaned up stuff

--- src/components/FetchComponent.vue
+++ src/components/FetchComponent.vue
@@ -6,30 +6,37 @@
                       :state="tickerState"
                       trim
                       aria-describedby="input-live-help input-live-feedback"
-                      @keyup.enter="fetchData"
+                      @keyup.enter="fetchData(search)"
         ></b-form-input>
-        <!-- This will only be shown if the preceding input has an invalid state -->
         <b-form-invalid-feedback id="input-live-feedback">
-          Enter a ticker listed on NYSE / NASDAQ.
+          {{ this.errorMsg }}
         </b-form-invalid-feedback>
       </b-input-group>
     </label>
-    <br>
-    <br>
-
-    <div class="grid-container">
-      <div class="Logo">
-        <img class="object3" :src="profile.logo" alt="" width="120"/>
+    <div v-if="this.price && tickerState">
+      <hr>
+      <h3>
+        ({{ profile["symbol"] }}) {{ profile["name"] }}
+      </h3>
+      <br>
+      <div class="grid-container">
+        <div class="Logo" style="margin: 0 20px">
+          <img class="object3" :src="profile.logo" alt="" width="120"/>
+        </div>
+        <div class="Details" style="width: 550px; text-align: start">
+          <h2 style="float:left; white-space:nowrap;overflow:hidden;text-overflow:ellipsis;"
+              id="pricewithcolordollars">
+            ${{ this.price.split(".")[0] }}
+          </h2>
+          <h5 style="padding-top: 1px" id="pricewithcolorcents">
+            <span :style="priceColor">{{ this.price.split(".")[1] }}</span>
+          </h5>
+          <hr style="margin-top: 25px">
+          {{ profile["description"] }}
+        </div>
       </div>
-      <div class="Details" v-if="this.price && tickerState">
-        {{profile["name"] }} <br>
-        <hr>
-        {{profile["description"] }} <br>
-        <hr>
-        <div id="pricewithcolor">{{profile["exchange"] + " Price: " + this.price }}</div>
-      </div>
+      <hr>
     </div>
-
   </div>
 </template>
 
@@ -46,67 +53,48 @@ export default {
     return {
       search: "",
       price: null,
+      priceColor: null,
       error: false,
+      errorMsg: null,
       profile: "",
       interval: null
     }
   },
   methods: {
-    async fetchData() {
-      clearInterval(this.interval);
-
+    async fetchData(search) {
       this.price = null;
-      this.fetchPrice();
-
-      this.interval = setInterval(() => {
-        this.fetchPrice();
-      }, 10000);
-
-      await fetch("https://api.polygon.io/v1/meta/symbols/" + this.search + "/company?&apiKey=" + process.env.VUE_APP_POLYGON_API_KEY)
+      await fetch("https://api.polygon.io/v1/meta/symbols/" + search.toUpperCase() +
+          "/company?&apiKey=" + process.env.VUE_APP_POLYGON_API_KEY)
           .then(response => response.json())
           .then(data => {
             if (data["status"] !== "ERROR")
               this.profile = data;
           });
-    },
-    fetchPrice() {
-      fetch("https://api.twelvedata.com/price?symbol=" + this.search + "&apikey=" + process.env.VUE_APP_TWELVEDATA_API_KEY)
+      await fetch("https://api.twelvedata.com/price?symbol=" + search.toUpperCase() +
+          "&apikey=" + process.env.VUE_APP_TWELVEDATA_API_KEY)
           .then(response => response.json())
           .then(data => {
             if (data["code"] === 429) {
               this.error = true;
+              this.errorMsg = "Too many calls at once..."
               this.price = "loading...";
-            } else if (data["code"] === 400)
+            } else if (data["code"] === 400) {
               this.error = true;
-            else if (data["code"] >= 400) {
+              this.errorMsg = "Enter a ticker listed on NYSE / NASDAQ.";
+            } else if (data["code"] >= 400) {
               this.error = true;
-              this.price = "Error calling price data";
+              this.errorMsg = "Error.";
             } else {
               this.error = false;
-              this.price = '$' + data["price"].slice(0, -3);
-            }
-
-            if(this.error){
-              clearInterval(this.interval);
+              this.price = data["price"].slice(0, -3);
             }
           });
     }
-  },
-
+  }
 }
 </script>
 
 <style scoped>
-
-.Logo {
-  margin: 0 20px;
-}
-
-.Details {
-  width: 500px;
-  text-align: start;
-}
-
 hr {
   background-color: white;
 }
